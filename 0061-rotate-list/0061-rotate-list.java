52ab197eb0e41045bf27dfe20f00ed8dee0b8fe9
Pratik Kumar <pratiikkumarr@gmail.com>
2026-08-13 21:34:08 +0530

    Time: 0 ms (100%), Space: 44.2 MB (81.06%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0) return head;
+        int n=1; // to calculate length of LL
+        ListNode tail = head;
+        while(tail.next!=null){
+            tail = tail.next;
+            n++;
+        }
+        k = k%n;
+        if(k==0) return head;
+        tail.next = head;
+        ListNode newTail = head;
+        for(int i=1; i<n-k; i++){
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
+    }
+}
